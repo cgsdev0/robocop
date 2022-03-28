4ea7fd45f26a42e613dbc924a5181c7e96ab8a5d
Shane Schulte <shane.schulte@samsara.com>
2022-03-28 00:59:57 -0700

    add env

---
+++ index.js
@@ -0,0 +1,49 @@
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { Client, Intents } from "discord.js";
+
+const BOT_AUTH_TOKEN = process.env.BOT_AUTH_TOKEN;
+const BOT_CLIENT_ID = process.env.BOT_CLIENT_ID;
+const BOT_GUILD_ID = process.env.BOT_GUILD_ID;
+
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+];
+
+const rest = new REST({ version: "9" }).setToken(BOT_AUTH_TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(
+      Routes.applicationGuildCommands(BOT_CLIENT_ID, BOT_GUILD_ID),
+      {
+        body: commands,
+      }
+    );
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(BOT_AUTH_TOKEN);
